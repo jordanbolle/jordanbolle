2f6d3c3a3a76cb4fd906863c8450f7dfd5580fa2
jordan <st10477387@vcconnect.edu.za>
2025-04-15 11:45:17 +0200

    Add files via upload

---
+++ Ice_task_1.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.ice_task_1;
+
+public class Ice_task_1 {
+
+    public static void main(String[] args) {
+        String Name;
+        String Surname;
+        String Student_Number;
+
+        Name = "Jordan";
+        Surname = "Bolle";
+        Student_Number = "ST10477387";
+
+        String sentence = "My name is " + Name + " " + Surname + " and my student number is " + Student_Number;
+
+        System.out.println(sentence);
+    }
+}
+//ST10477387
